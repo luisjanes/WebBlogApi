b7a871440703dff802f584ec0897b9d803935ab7
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2024-02-08 17:55:25 -0300

    feat: add try catch CategoryController

--- WebBlog/Controllers/CategoryController.cs
+++ WebBlog/Controllers/CategoryController.cs
@@ -11,18 +11,34 @@ namespace WebBlog.Controllers
         [HttpGet("v1/categories")]
         public async Task<IActionResult> GetAsync([FromServices] BlogDataContext _context)
         {
-            var categories = await _context.Categories.ToListAsync();
-            return Ok(categories);
+            try
+            {
+                var categories = await _context.Categories.ToListAsync();
+                return Ok(categories);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "01X01 - Couldn't get this Categories");
+            }
+
         }
         [HttpGet("v1/categories/{id:int}")]
         public async Task<IActionResult> GetByIdAsync([FromRoute]int id, [FromServices] BlogDataContext _context)
         {
-            var category = await _context.Categories.FirstOrDefaultAsync(x=>x.Id == id);
-            if (category == null)
+            try
             {
-                return NotFound($"With Id: {id}, couldn't find any Category");
+                var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                {
+                    return NotFound($"With Id: {id}, couldn't find any Category");
+                }
+                return Ok(category);
             }
-            return Ok(category);
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "01X01 - Couldn't get this Category");
+            }
+
         }
         [HttpPost("v1/categories")]
         public async Task<IActionResult> PostAsync([FromBody] Category model,[FromServices] BlogDataContext _context)
@@ -45,28 +61,52 @@ namespace WebBlog.Controllers
         [HttpPut("v1/categories/{id:int}")]
         public async Task<IActionResult> PutAsync([FromRoute] int id, [FromServices] BlogDataContext _context, [FromBody]Category model)
         {
-            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-            if (category == null)
+            try
             {
-                return NotFound($"With Id: {id}, couldn't find any Category");
+                var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                {
+                    return NotFound($"With Id: {id}, couldn't find any Category");
+                }
+                category.Name = model.Name;
+                category.Slug = model.Slug;
+                _context.Categories.Update(category);
+                await _context.SaveChangesAsync();
+                return Ok($"The updates are made {category}");
             }
-            category.Name = model.Name;
-            category.Slug = model.Slug;
-            _context.Categories.Update(category);
-            await _context.SaveChangesAsync();
-            return Ok($"The updates are made {category}");
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "01X01 - Couldn't update this Category");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal faulure");
+            }
+
         }
         [HttpDelete("v1/categories/{id:int}")]
         public async Task<IActionResult> DeleteAsync([FromRoute] int id, [FromServices] BlogDataContext _context)
         {
-            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-            if (category == null)
+            try
             {
-                return NotFound($"With Id: {id}, couldn't find any Category");
+                var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                {
+                    return NotFound($"With Id: {id}, couldn't find any Category");
+                }
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+                return Ok($"{category.Name} was deleted");
             }
-            _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
-            return Ok($"{category.Name} was deleted");
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "01X01 - Couldn't delete this Category");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal faulure");
+            }
+
         }
     }
 }
