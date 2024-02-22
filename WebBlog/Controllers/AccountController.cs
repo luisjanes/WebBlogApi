657747284f4f914a66c2af0a78de8bb30913e17f
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2024-02-22 16:33:00 -0300

    feat: add login controller, user create controller

--- WebBlog/Controllers/AccountController.cs
+++ WebBlog/Controllers/AccountController.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
+using WebBlog.Data;
+using WebBlog.Extensions;
+using WebBlog.Models;
 using WebBlog.Services;
+using WebBlog.ViewModels;
 
 namespace WebBlog.Controllers
 {
@@ -8,25 +14,61 @@ namespace WebBlog.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        [HttpPost("v1/accounts/")]
+        public async Task<IActionResult> Post([FromBody]RegisterViewModel model, [FromServices] BlogDataContext _context)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+            }
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@","-").Replace(".","-"),
+            };
+
+            var password = PasswordGenerator.Generate(25);
+            user.PasswordHash= PasswordHasher.Hash(password);
+
+            try
+            {
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    user = user.Email, password
+                }));
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(400, new ResultViewModel<string>("Email já cadastrado, tente fazer login"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>("Erro interno de servidor"));
+            }
+        }
         //[AllowAnonymous] para permitir que utilizem sem autenticação
         [HttpPost("v1/login")]
-        public IActionResult Login([FromServices]TokenService tokenService)
+        public IActionResult Login([FromServices] TokenService tokenService)
         {
             var token = tokenService.GenerateToken(null);
 
             return Ok(token);
         }
 
-        [Authorize(Roles = "user")]
-        [HttpGet("v1/user")]
-        public IActionResult GetUser() => Ok(User.Identity.Name);
+        //[Authorize(Roles = "user")]
+        //[HttpGet("v1/user")]
+        //public IActionResult GetUser() => Ok(User.Identity.Name);
 
-        [Authorize(Roles = "author")]
-        [HttpGet("v1/author")]
-        public IActionResult GetAuthor() => Ok(User.Identity.Name);
+        //[Authorize(Roles = "author")]
+        //[HttpGet("v1/author")]
+        //public IActionResult GetAuthor() => Ok(User.Identity.Name);
 
-        [Authorize(Roles = "admin")]
-        [HttpGet("v1/admin")]
-        public IActionResult GetAdmin() => Ok(User.Identity.Name);
+        //[Authorize(Roles = "admin")]
+        //[HttpGet("v1/admin")]
+        //public IActionResult GetAdmin() => Ok(User.Identity.Name);
     }
 }
