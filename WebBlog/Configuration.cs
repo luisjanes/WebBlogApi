fbf811ef2cb10b8156a0a98f56d89671a192f8d3
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2024-02-28 11:48:49 -0300

    feat: add appsettings configuration

--- WebBlog/Configuration.cs
+++ WebBlog/Configuration.cs
@@ -5,5 +5,13 @@
         public static string JwtKey = "MzE2MmQwY2ItYjZlOS00ZDQyLWEzNjMtYWM4YjA0MDc3YzJk";
         public static string ApiKeyName = "api_key";
         public static string ApiKey = "curso_api_lSFalQlkUa2mOBw1s4JxA==";
+        public static SmtpConfiguration Smtp = new();
+        public class SmtpConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; } = 25;
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
